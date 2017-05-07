db68711d47045931c959f67a4c0919a5506cec24
张智全 <zhangzhiquan@alibaba-inc.com>
2017-05-07 18:33:14 +0800

    剑指offer练习题

--- ExerciseApplication/src/sword/MainTest.java
+++ ExerciseApplication/src/sword/MainTest.java
@@ -7,10 +7,22 @@ import java.util.List;
  */
 public class MainTest {
     public static void main(String[] args){
+        testPermutation();
+        testMoreThanHalfNum();
+
+    }
+
+    private static void testPermutation(){
         SolutionPermutation solutionPermutation = new SolutionPermutation();
         List<String> res = solutionPermutation.Permutation("aaa");
         for(String str : res){
             System.out.println(str);
         }
+    }
+
+    private static void testMoreThanHalfNum(){
+        SolutionMoreThanHalfNum solution = new SolutionMoreThanHalfNum();
+        int[] num = new int[]{1,2,3,2,4,2,5,2,3};
+        int ans = solution.MoreThanHalfNum_Solution(num);
     }
 }
