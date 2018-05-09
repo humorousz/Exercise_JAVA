f7d21966943d6fc2f7858ebc87d7f10a8a99c4dc
zhangzhiquan <zhanzhiquan@alibaba-inc.com>
2018-05-09 11:37:03 +0800

    leetCode

--- ExerciseApplication/src/leetCode/SolutionSubsets.java
+++ ExerciseApplication/src/leetCode/SolutionSubsets.java
@@ -7,25 +7,13 @@ import java.util.*;
  * Given a set of distinct integers, S, return all possible subsets.
  */
 public class SolutionSubsets {
-    public ArrayList<ArrayList<Integer>> subsets(int[] S) {
+    public ArrayList<ArrayList<Integer>> subsets(int[] num) {
         ArrayList<ArrayList<Integer>> res = new ArrayList<>();
         ArrayList<Integer> tmp = new ArrayList<>();
-        Arrays.sort(S);
+        Arrays.sort(num);
         res.add(tmp);
-        dfs(res,tmp,S,0);
-        Collections.sort(res, new Comparator<ArrayList<Integer>>() {
-            @Override
-            public int compare(ArrayList<Integer> o1, ArrayList<Integer> o2) {
-                if(o1.size() == o2.size()){
-                    return 0;
-                }
-                if(o1.size() > o2.size()){
-                    return 1;
-                }else {
-                    return -1;
-                }
-            }
-        });
+        dfs(res,tmp,num,0);
+
         return res;
     }
 
@@ -35,6 +23,9 @@ public class SolutionSubsets {
             res.add(new ArrayList<Integer>(tmp));
             dfs(res,tmp,S,i+1);
             tmp.remove(tmp.size()-1);
+            while (i<S.length - 1 && S[i] == S[i + 1]){
+                i++;
+            }
         }
     }
 
@@ -55,7 +46,7 @@ public class SolutionSubsets {
 
     public static void  main(String[] args){
         SolutionSubsets solutionSubsets = new SolutionSubsets();
-        ArrayList<ArrayList<Integer>> subsets = solutionSubsets.subsets(new int[]{1,2,3,4});
+        ArrayList<ArrayList<Integer>> subsets = solutionSubsets.subsets(new int[]{1,2});
         for(int i = 0;i<subsets.size();i++){
             for(int j = 0 ;j < subsets.get(i).size();j++){
                 System.out.print(subsets.get(i).get(j)+" ");
