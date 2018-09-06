fcf9b4b086ff2addc50053c3d1bf9dab0a32794b
张智全 <zhangzhiquan@alibaba-inc.com>
2018-09-06 13:21:22 +0800

    剑指offer

--- ExerciseApplication/src/sword/PrintList.java
+++ ExerciseApplication/src/sword/PrintList.java
@@ -1,5 +1,7 @@
 package sword;
 
+import java.awt.*;
+
 /**
  * Created by zhangzhiquan on 2018/3/13.
  */
@@ -22,7 +24,7 @@ public class PrintList {
         }
         PrintList obj = new PrintList();
         obj.printList(node);
-        obj.printList(obj.f(node));
+        obj.printList(obj.nf2(node));
     }
 
 
@@ -57,4 +59,49 @@ public class PrintList {
         newHead = head;
         return newHead;
     }
+
+    ListNode nf(ListNode node){
+        if(node == null || node.next == null){
+            return node;
+        }
+        ListNode newHead = null;
+        while (node != null){
+            ListNode tmp = node;
+            node = node.next;
+            tmp.next = newHead;
+            newHead = tmp;
+        }
+        return newHead;
+    }
+
+    ListNode nf2(ListNode node){
+        if(node == null || node.next == null){
+            return node;
+        }
+        ListNode newHead = null;
+        while (node != null){
+            ListNode tmp = node.next;
+            node.next = newHead;
+            newHead = node;
+            node = tmp;
+        }
+        return newHead;
+    }
+
+    ListNode df(ListNode node){
+        if(node == null || node.next == null){
+            return node;
+        }
+        ListNode fff = node.next;
+        node.next = null;
+        ListNode newHead = df(fff);
+        ListNode tmp =newHead;
+        while (tmp .next !=null){
+            tmp= tmp.next;
+        }
+        tmp.next = node;
+        return newHead;
+    }
+
+
 }
--- ExerciseApplication/src/sword/PrintTree.java
+++ ExerciseApplication/src/sword/PrintTree.java
@@ -22,6 +22,22 @@ public class PrintTree {
 
     public static void main(String[] args){
         PrintTree obj = new PrintTree();
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node5 = new TreeNode(5);
+        TreeNode node6 = new TreeNode(6);
+        TreeNode node7 = new TreeNode(7);
+        TreeNode node8 = new TreeNode(8);
+        node1.left = node2;
+        node1.right = node3;
+        node2.left = node4;
+        node2.right = node5;
+        node5.right = node6;
+        node6.left = node7;
+        node6.right = node8;
+        System.out.print(obj.deep(node1));
     }
 
 
@@ -90,5 +106,13 @@ public class PrintTree {
             }
         }
         return res;
+    }
+
+
+    public int deep(TreeNode node){
+        if(node == null){
+            return 0;
+        }
+        return Math.max(deep(node.left),deep(node.right))+1;
     }
 }
